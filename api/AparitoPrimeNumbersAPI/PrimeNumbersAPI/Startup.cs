d183f05a40e753dd3996079c3c51662fc9792456
Andrew Pillow-Edwards <loophole.edwards@gmail.com>
2021-01-30 16:01:28 +0000

    Add environment variable to fix CORS issues in docker

--- api/AparitoPrimeNumbersAPI/PrimeNumbersAPI/Startup.cs
+++ api/AparitoPrimeNumbersAPI/PrimeNumbersAPI/Startup.cs
@@ -28,12 +28,13 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddCors(options =>
             {
                 options.AddPolicy(name: DEVELOPMENT_CORS_POLICY,
                     builder =>
                     {
-                        builder.WithOrigins("http://localhost:3000")
+                        builder.WithOrigins(Environment.GetEnvironmentVariable("DEVELOPMENT_CORS_ORIGIN"))
                             .AllowAnyMethod()
                             .AllowAnyHeader();
                     });
